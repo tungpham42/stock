4bda7b8fc4cb9e4faffd70145b307bc4019c4bf4
Tung Pham <tung.42@gmail.com>
2025-12-11 16:18:06 +0700

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,12 +1,13 @@
-const fetch = require("node-fetch");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 exports.handler = async function (event, context) {
   try {
-    // event.path contains the path after /.netlify/functions/proxy
     const proxiedPath = event.path.replace(
       /^\/\.netlify\/functions\/proxy/,
       ""
     );
+
     const targetUrl = `https://apipubaws.tcbs.com.vn${proxiedPath}${
       event.rawQuery ? `?${event.rawQuery}` : ""
     }`;
@@ -19,18 +20,20 @@ exports.handler = async function (event, context) {
     const resp = await fetch(targetUrl, { headers });
     const body = await resp.text();
 
-    // forward status and headers (but avoid unsafe headers)
     return {
       statusCode: resp.status,
       headers: {
         "Content-Type": resp.headers.get("content-type") || "application/json",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        "Access-Control-Allow-Methods": "GET,OPTIONS",
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
       },
       body,
     };
   } catch (err) {
-    return { statusCode: 500, body: JSON.stringify({ error: String(err) }) };
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: String(err) }),
+    };
   }
 };
